11e472ab0f73abe454171b423356008b13fad0c9
Dhartharis <hansmolina989@gmail.com>
2026-03-03 21:59:38 +0700

    Stop tracking obj folder

--- src/OrderProcessing.Worker/Program.cs
+++ src/OrderProcessing.Worker/Program.cs
@@ -1,13 +1,13 @@
-using OrderProcessing.Infrastructure.Orders;
-using Microsoft.EntityFrameworkCore;
+using OrderProcessing.Infrastructure;
+using OrderProcessing.Application;
 
 var builder = Host.CreateApplicationBuilder(args);
 
 //builder.Configuration.AddJsonFile("appsettings.json", optional: false);
 
-builder.Services.AddDbContext<OrderDbContext>(options => 
-    options.UseNpgsql(builder.Configuration.GetConnectionString("Default"))
-);
+builder.Services
+    .AddApplication()
+    .AddInfrastructure(builder.Configuration);
 
 builder.Services.AddHostedService<OrdersProcessingWorker>();
 
